0b43bca3b285967122ae20cc3154ff71d8ba96d0
masudurrahmannirob <masudurrahmannirob@gmail.com>
2025-11-01 02:40:34 +0000

    up

--- lib/features/authentication/screens/welcome_screen.dart
+++ lib/features/authentication/screens/welcome_screen.dart
@@ -1,5 +1,8 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -7,27 +10,45 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome to MyApp'),
-      ),
+      backgroundColor: const Color(0xFFF0F4F0),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Welcome to MyApp!',
+            Text(
+              'Easy Farm\nEquipment Rentals',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.lato(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xFF2E7D32),
+              ),
             ),
+            const SizedBox(height: 40),
+            SvgPicture.asset(
+              'assets/images/tractor.svg',
+              height: 200,
+            ),
+            const SizedBox(height: 60),
             ElevatedButton(
               onPressed: () {
                 context.go('/login');
               },
-              child: const Text('Login'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.go('/signup');
-              },
-              child: const Text('Sign Up'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF2E7D32),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 16),
+              ),
+              child: Text(
+                'Get Started',
+                style: GoogleFonts.lato(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ],
         ),
