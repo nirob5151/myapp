b329dd6efb0827b274f728efed0f60202845323e
masudurrahmannirob <masudurrahmannirob@gmail.com>
2025-11-26 06:11:08 +0000

    up

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,17 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/providers/theme_provider.dart';
 import 'package:myapp/utils/routes.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await updateDatabase();
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
@@ -13,6 +22,37 @@ void main() {
     ),
   );
 }
+Future<void> updateDatabase() async {
+  final firestore = FirebaseFirestore.instance;
+  final equipmentCollection = firestore.collection('equipment');
+
+  final equipmentSnapshot = await equipmentCollection.get();
+
+  for (final doc in equipmentSnapshot.docs) {
+    final data = doc.data();
+    final name = data['name'] as String;
+    String category;
+
+    if (name.toLowerCase().contains('tractor')) {
+      category = 'Tractors';
+    } else if (name.toLowerCase().contains('harvester')) {
+      category = 'Harvesters';
+    } else if (name.toLowerCase().contains('planter')) {
+      category = 'Planters';
+    } else if (name.toLowerCase().contains('sprayer')) {
+      category = 'Sprayers';
+    } else if (name.toLowerCase().contains('plow')) {
+      category = 'Plows';
+    } else {
+      category = 'Other'; // Default category
+    }
+
+    await doc.reference.update({'category': category});
+  }
+
+  print('Database update complete!');
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
